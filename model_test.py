e77e43b96d1f757b8e19ab6a2947e5161ad9196a
Arby Gudes <aegudes@gmail.com>
2025-12-07 21:32:31 -0600

    fuses camtest into model_test

--- model_test.py
+++ model_test.py
@@ -1,10 +1,22 @@
 from ultralytics import YOLO
+from picamera2 import Picamera2
 import os
+import time
 
 home_dir = os.environ["HOME"]
+cam = Picamera2()
+config = cam.create_still_configuration()
+cam.configure(config)
+
+cam.start()
+print("Taking Picture..")
+time.sleep(2)
+cam.capture_file(f"{home_dir}/EV-Charging-Arm/current.jpg")
+print("Saved Picture")
 
 # Load trained model
 model = YOLO(r"ev_socket_model.pt")
+print("Finding Socket..")
 
 # Run inference with boxes automatically drawn & saved
-results = model("current.jpg", name=f"{home_dir}/EV-Charging-Arm")
+results = model("current.jpg", save=True, name=".")
