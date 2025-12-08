167925e6b7880e1cda7353116f92affd5b7b58f0
Arby Gudes <aegudes@gmail.com>
2025-12-07 21:06:55 -0600

    updates cam_test, updates model_test

--- model_test.py
+++ model_test.py
@@ -1,7 +1,10 @@
 from ultralytics import YOLO
+import os
+
+home_dir = os.environ["HOME"]
 
 # Load trained model
 model = YOLO(r"ev_socket_model.pt")
 
 # Run inference with boxes automatically drawn & saved
-results = model("test02.jpg", save=True, show=True, name="output")
+results = model("test02.jpg", name=f"{home_dir}/EV-Charging-Arm")
