ad5c43c52ed740f037a42ffd9ee713ca6eecf339
Arby Gudes <58876002+HakaseKorin@users.noreply.github.com>
2026-05-03 16:52:02 -0500

    prepares cam_test for image collection

--- Homebrew_Version/cam_test.py
+++ Homebrew_Version/cam_test.py
@@ -1,6 +1,9 @@
 from picamera2 import Picamera2
-import os
 import time
+import os
+import random
+
+photo_id = random.randint(10000000,99999999)
 
 home_dir = os.environ["HOME"]
 cam = Picamera2()
@@ -9,5 +12,5 @@ cam.configure(config)
 
 cam.start()
 time.sleep(2)
-cam.capture_file(f"{home_dir}/EV-Charging-Arm/current.jpg")
-print("Saved current.jpg")
+cam.capture_file(f"{home_dir}/EV-Charging-Arm/current{photo_id}.jpg")
+print(f"Saved current{photo_id}.jpg")
